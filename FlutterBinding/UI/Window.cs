fe479bb8132fd2c53acdd77acfaa3ade425d5ff6
Adam Pedley <adam.pedley@gmail.com>
2018-09-30 12:22:51 +1000

    Example of FlutterBinding project

---
+++ FlutterBinding/UI/Window.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlutterBinding.UI
+{
+    public class Window
+    {
+        public object onBeginFrame { get; set; }
+        public object onDrawFrame { get; set; }
+    }
+}
