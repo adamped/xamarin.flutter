af807d4c872cfdb5ffa8c28b61aa3b214001e1d3
Adam Pedley <adam.pedley@gmail.com>
2018-09-30 14:25:11 +1000

    Sample Mainpage

---
+++ Flutter.Sample.App/MainPage.cs
@@ -0,0 +1,89 @@
+﻿namespace Flutter.Sample.App
+{
+    // Left here as a sample of what this may look like
+
+    //static void main() => runApp(new MyApp());
+
+    //public class MyApp : StatelessWidget
+    //{
+    //    // This widget is the root of your application.
+    //    public override Widget build(BuildContext context)
+    //    {
+    //        return new MaterialApp(
+    //          title: "Flutter Demo",
+    //          theme: new ThemeData(
+    //            // This is the theme of your application.
+    //            //
+    //            // Try running your application with "flutter run". You'll see the
+    //            // application has a blue toolbar. Then, without quitting the app, try
+    //            // changing the primarySwatch below to Colors.green and then invoke
+    //            // "hot reload" (press "r" in the console where you ran "flutter run",
+    //            // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
+    //            // counter didn't reset back to zero; the application is not restarted.
+    //            primarySwatch: Colors.blue
+    //          ),
+    //          home: new MyHomePage(title: "Flutter Demo Home Page")
+    //        );
+    //    }
+    //}
+
+    //public class MyHomePage : StatefulWidget
+    //{
+    //    public MyHomePage(string title, Key key = null) : base(key: key)
+    //    {
+    //        this.title = title;
+    //    }
+
+    //    // This widget is the home page of your application. It is stateful, meaning
+    //    // that it has a State object (defined below) that contains fields that affect
+    //    // how it looks.
+
+    //    // This class is the configuration for the state. It holds the values (in this
+    //    // case the title) provided by the parent (in this case the App widget) and
+    //    // used by the build method of the State. Fields in a Widget subclass are
+    //    // always marked "final".
+
+    //    public readonly string title;
+    //    public override State createState() => new _MyHomePageState();
+    //}
+
+    //public class _MyHomePageState : State<MyHomePage>
+    //{
+    //    int _counter = 0;
+
+    //    void _incrementCounter()
+    //    {
+    //        setState(() =>
+    //        {
+    //            // This call to setState tells the Flutter framework that something has
+    //            // changed in this State, which causes it to rerun the build method below
+    //            // so that the display can reflect the updated values. If we changed
+    //            // _counter without calling setState(), then the build method would not be
+    //            // called again, and so nothing would appear to happen.
+    //            _counter++;
+    //        });
+    //    }
+
+    //    public override Widget build(BuildContext context)
+    //    {
+    //        // This method is rerun every time setState is called, for instance as done
+    //        // by the _incrementCounter method above.
+    //        //
+    //        // The Flutter framework has been optimized to make rerunning build methods
+    //        // fast, so that you can just rebuild anything that needs updating rather
+    //        // than having to individually change instances of widgets.
+    //        return new Scaffold(
+    //          appBar: new AppBar(
+    //            // Here we take the value from the MyHomePage object that was created by
+    //            // the App.build method, and use it to set our appbar title.
+    //            title: new Text(widget.title)
+    //          ),
+    //          body: new Center(
+    //            // Center is a layout widget. It takes a single child and positions it
+    //            // in the middle of the parent.
+    //            child: new Text("Hello World!")
+    //          )
+    //        );
+    //    }
+    //}
+}
