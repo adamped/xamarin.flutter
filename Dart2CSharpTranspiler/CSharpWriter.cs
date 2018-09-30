44bdf72fd75f68cb05e599429c642215788a404c
Adam Pedley <adam.pedley@gmail.com>
2018-09-30 12:51:21 +1000

    Sample shell projects

--- Dart2CSharpTranspiler/CSharpWriter.cs
+++ Dart2CSharpTranspiler/CSharpWriter.cs
@@ -12,12 +12,12 @@ namespace Transpiler
 
         public static string CreateFile(DartFile dart, DartModel model)
         {
-            var @namespace = SyntaxFactory.NamespaceDeclaration(SyntaxFactory.ParseName(dart.Folder.ToPascalCasing()))
+            var @namespace = SyntaxFactory.NamespaceDeclaration(SyntaxFactory.ParseName(dart.Folder))
                                           .NormalizeWhitespace();
 
             foreach (var @class in dart.Classes)
             {
-                var classDeclaration = SyntaxFactory.ClassDeclaration(@class.Name.ToPascalCasing());
+                var classDeclaration = SyntaxFactory.ClassDeclaration(@class.Name);
 
                 classDeclaration = classDeclaration.AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword));
 
@@ -31,31 +31,6 @@ namespace Transpiler
                .ToFullString();
 
             return code;
-        }
-
-        static string ToPascalCasing(this string value)
-        {
-            var resultBuilder = new System.Text.StringBuilder();
-            
-            // Replace anything, but letters and digits, with space
-            foreach (char c in value)
-            {
-                if (!Char.IsLetterOrDigit(c))
-                {
-                    resultBuilder.Append(" ");
-                }
-                else
-                {
-                    resultBuilder.Append(c);
-                }
-            }
-
-            string result = resultBuilder.ToString();
-
-            result = result.ToLower();
-
-            var textInfo = new CultureInfo("en-US", false).TextInfo;
-            return textInfo.ToTitleCase(result).Replace(" ", string.Empty);            
         }
     }
 }
