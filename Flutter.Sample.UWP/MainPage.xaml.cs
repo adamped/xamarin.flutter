2aff7ab3c1e419c18ad78120e8977a9f58c2e03d
brainoffline <1763877+brainoffline@users.noreply.github.com>
2018-10-02 00:04:55 +1300

    Initial working UWP Flutter page + SkiaSharp

--- Flutter.Sample.UWP/MainPage.xaml.cs
+++ Flutter.Sample.UWP/MainPage.xaml.cs
@@ -20,7 +20,7 @@ namespace Flutter.Sample.UWP
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage
     {
         public MainPage()
         {
