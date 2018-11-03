81e3b8f3ab19981beb7eadc9aa6abda4cc3cfe4f
edgardev <edgar.gonzalezpena@aviva.com>
2018-11-03 17:29:05 +0000

    Add FlutterCanvas Activity to Shell Project

---
+++ Flutter.Shell.Droid/FlutterCanvas.cs
@@ -0,0 +1,41 @@
+﻿using Android.OS;
+using Android.Support.V7.App;
+using FlutterBinding.UI;
+using SkiaSharp.Views.Android;
+
+namespace Flutter.Shell.Droid
+{
+    public class FlutterCanvas : AppCompatActivity
+    {
+        public SKCanvasView Canvas { get; private set; }
+        public FlutterSurface Surface { get; private set; }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.FlutterPage);
+
+            Canvas = FindViewById<SKCanvasView>(Resource.Id.skiaView);
+
+            var scale = Resources.DisplayMetrics.Density;
+
+            if (Canvas != null)
+            {
+                Surface = new FlutterSurface(scale);
+            }
+
+            Canvas.PaintSurface += OnCanvasPaintSurface;
+        }
+
+        private void OnCanvasPaintSurface(object sender, SKPaintSurfaceEventArgs args)
+        {
+            Surface.OnPaintSurface(args.Surface, args.Info);
+        }
+
+        protected override void OnDestroy()
+        {
+            Canvas.PaintSurface -= OnCanvasPaintSurface;
+            base.OnDestroy();
+        }
+    }
+}
