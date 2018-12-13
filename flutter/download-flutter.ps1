8f0698247e34024055553ec320b8026b4551c5da
Konrad Müller <k.l.mueller@outlook.com>
2018-12-13 02:18:11 +0100

    added flutter source download script

---
+++ flutter/download-flutter.ps1
@@ -0,0 +1,23 @@
+echo "test"
+$webclient = New-Object System.Net.WebClient
+$url = "https://github.com/flutter/flutter/archive/master.zip"
+$zip = "flutter.zip"
+echo "download $url to $zip"
+$webclient.DownloadFile($url,$zip)
+echo "finished download"
+
+echo "unzipping file"  
+$subfolder = 'flutter-master\packages\flutter'  # path in the zip
+$shellApp = New-Object -ComObject Shell.Application 
+$path = $shellApp.namespace("$PSScriptRoot\$zip\$subfolder") # complete subfolder path
+$destination = $shellApp.namespace("$PSScriptRoot") #destination
+$destination.CopyHere($path)
+
+Move-Item -Path "flutter\*" -Destination ""
+
+echo "cleaning up"
+rm -r -fo "flutter"
+rm $zip
+echo "finished downloading flutter" 
+
+flutter packages get
