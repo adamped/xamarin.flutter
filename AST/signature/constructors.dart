86d866e2c2bac32071c46353412b5c1a09837a60
Konrad Müller <k.l.mueller@outlook.com>
2018-12-24 03:41:53 +0100

    factory method generation for named ctors; ctor base parameters

--- AST/signature/constructors.dart
+++ AST/signature/constructors.dart
@@ -1,69 +1,100 @@
+import 'package:analyzer/analyzer.dart';
 import 'package:analyzer/dart/element/element.dart';
-
+import 'package:analyzer/src/dart/element/element.dart';
+import 'package:analyzer/dart/ast/ast.dart';
 import '../implementation/implementation.dart';
 import '../naming.dart';
 import 'methods.dart';
 import '../config.dart';
+import 'package:front_end/src/scanner/token.dart';
 
 class Constructors {
   static void printConstructor(
       StringBuffer code, ConstructorElement constructor, String generics) {
     if (constructor.enclosingElement is ClassElement) {
-      var isStatic = false;
+      var isFactory = false;
       var className = constructor.enclosingElement.name;
+      var constructorName = constructor.name;
+      var callsBaseCtor = constructor.redirectedConstructor != null;
 
       var parameters = Methods.printParameter(constructor, null, null, null);
-      if (constructor.name == '')
+      // normal constructors do not have any special key chars
+      if (constructorName == '')
         code.writeln('public ${className}($parameters)');
-      else if (constructor.name == '_')
-        code.writeln('internal ${className}($parameters)');
-      else if (constructor.name.startsWith('_'))
+      // internal classes start with an underscore in dart
+      else if (constructorName == '_' || constructorName.startsWith('_'))
         code.writeln('internal ${className}($parameters)');
       else // I'm named, hence we are turing into static methods that return an instance
       {
-        isStatic = true;
-        code.writeln('private ${className}($parameters)');
+        isFactory = true;
+        code.writeln(
+            'public static ${className}$generics ${Naming.upperCamelCase(constructorName)}($parameters)');
       }
 
       // Base class call
-      if (constructor.redirectedConstructor != null) {
-        var baseCall = constructor.redirectedConstructor;
-        var baseParameters = Methods.printParameterNames(baseCall);
-
-        code.writeln(': base($baseParameters)');
+      if (callsBaseCtor && !isFactory) {
+        code.writeln(': base(${getBaseParameters(constructor)})');
       }
+
+      var instanceName = isFactory ? "instance" : "this";
 
       // Fill out Constructor body
       var node = constructor.computeNode();
       if (node != null) {
-        var body = '';
+        var body = '{\n';
 
-        if (Config.includeConstructorImplementations)
-          body = Implementation.MethodBody(node.body);
-        else
-          body = '{ throw new NotImplementedException(); }';
+        if (isFactory) {
+          // Insert initialization if this is a factory method
+          body += 'var instance =';
+          if (callsBaseCtor) {
+            var parameters = getBaseParameters(constructor);
+            //TODO Get the correct constructor name in case this class does not call its own constructor!
+            body += 'new ${className}$generics(${parameters});';
+
+            if (className == "AsyncSnapshot") {
+              print("blub");
+            }
+          } else
+            body += 'new ${className}$generics();';
+        }
 
         // Add auto assignments if any
-        var autoAssignment = Methods.printAutoParameters(constructor, className);
-        if (autoAssignment.isNotEmpty)
-          body = '{\n' + autoAssignment + '\n' + body.substring(2);
+        var autoAssignment = Methods.printAutoParameters(constructor, className,
+            instanceName: instanceName);
+        if (autoAssignment.isNotEmpty) body += autoAssignment;
+
+        // add logic and closing curly brace
+        if (Config.includeConstructorImplementations)
+          body = Implementation.MethodBody(node.body).substring(2);
+        else
+          body += 'throw new NotImplementedException(); }';
 
         // Normal constructor body
         code.writeln(body);
-
-        if (isStatic) {
-          code.writeln(
-              'public static ${className}$generics ${Naming.upperCamelCase(constructor.name)}($parameters)');
-          var parameterNames = Methods.printParameterNames(constructor);
-          // Call private constructor
-          code.writeln(
-              '{\nvar instance = new ${className}$generics($parameterNames);\nreturn instance;\n}\n');
-        }
-        
       } else
         code.writeln('{ }');
     } else
       throw new AssertionError(
           'A constructor is not inside a ClassElement, that should not happen.');
+  }
+
+  static String getBaseParameters(ConstructorElement constructor) {
+    // Get parameters
+    var parameters = "";
+    if (constructor is ConstructorElementImpl &&
+        constructor.constantInitializers.length > 0) {
+      // :)
+      var constantInitializer = constructor.constantInitializers.first;
+      ArgumentList argumentList = constantInitializer.childEntities
+          .firstWhere((x) => x is ArgumentList, orElse: null);
+      if (argumentList != null) {
+        parameters = argumentList.childEntities
+            .where((argument) =>
+                argument is! BeginToken && argument is! SimpleToken)
+            .map((argument) => Implementation.processEntity(argument))
+            .join(",");
+      }
+    }
+    return parameters;
   }
 }
