ebcf86ef4f14419a23f3fe8a926bd08a2d2924ac
Adam Pedley <adam.pedley@gmail.com>
2018-12-31 22:45:47 +1100

    test update

--- testbed/test.dart
+++ testbed/test.dart
@@ -1,31 +1,17 @@
-class HitTestTarget {
+class HitTestTarget {}
 
-}
+mixin DiagnosticableTreeMixin {}
 
-mixin DiagnosticableTreeMixin {
-
-}
-
-class AbstractNode {
-
-}
+class AbstractNode {}
 
 abstract class RenderObject extends AbstractNode with DiagnosticableTreeMixin implements HitTestTarget
 {
   void debugResetSize() { }
 }
 
-abstract class RenderSliver extends RenderObject {
- 
+abstract class RenderSliver extends RenderObject { 
   @override
   void debugResetSize() { }
-
-  @protected
-  bool hitTestSelf({ @required double mainAxisPosition, @required double crossAxisPosition }) => false;
-
 }
 
-abstract class RenderSliverHelpers implements RenderSliver
-{
-  
-}
+abstract class RenderSliverHelpers implements RenderSliver { }
